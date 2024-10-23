b6420cb26c8250236e6fc0906b453dbdc5eb946c
munhugues <h.munezero@alustudent.com>
2024-10-23 12:10:16 -0700

    Showing the last digit of a nu,ber

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ last_num = abs(number) % 10
 if(number < 0):
     last_num = -last_num
 
-print(f"The last digit of {number} is ",end="")
+print(f"Last digit of {number} is ",end="")
 if((last_num < 5) & (last_num != 0)):
     print(f"{last_num} and is less than 6 and not 0")
 elif(last_num > 5):
