3a09248fa61f5484d9678798ba9644430583b08d
munhugues <h.munezero@alustudent.com>
2024-10-23 03:51:20 -0700

    1-last_digit.py

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_num = int(repr(number)[-1])
+if((last_num < 5) & (last_num != 0)):
+    print(f"The last digit of {number} is {last_num} and is less than 6 and not 0")
+elif(last_num > 5):
+    print(f"The last digit of {number} is {last_num} and is greater than 5")
+elif(last_num == 0):
+    print(f"The last digit of {number} and is {last_num}")
+
