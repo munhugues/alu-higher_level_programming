192e4e5f2b96e7ff94fbac88bf7f8b0fe656d1f7
munhugues <h.munezero@alustudent.com>
2024-10-23 12:08:24 -0700

    Showing the last digit of a nu,ber

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_num = int(repr(number)[-1])
-if(number > 0):
-    if((last_num < 5) & (last_num != 0)):
-        print(f"The last digit of {number} is {last_num} and is less than 6 and not 0")
-    elif(last_num > 5):
-        print(f"The last digit of {number} is {last_num} and is greater than 5")
-elif(number < 0):
-    if((last_num < 5) & (last_num != 0)):
-        print(f"The last digit of {number} is -{last_num} and is less than 6 and not 0")
-    elif(last_num > 5):
-        print(f"The last digit of {number} is -{last_num} and is greater than 5")
-else:
-    print(f"The last digit of {number} and is {last_num}")
+last_num = abs(number) % 10
+if(number < 0):
+    last_num = -last_num
 
+print(f"The last digit of {number} is ",end="")
+if((last_num < 5) & (last_num != 0)):
+    print(f"{last_num} and is less than 6 and not 0")
+elif(last_num > 5):
+    print(f"{last_num} and is greater than 5")
+else:
+    print(f"{last_num} and is 0")
