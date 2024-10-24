30c18a542886743b5addedd73af1e0b1c29fcd22
munhugues <h.munezero@alustudent.com>
2024-10-24 04:29:34 -0700

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for letter_code in range(ord('a'), ord('z')+1):
     letter = chr(letter_code)
-    if letter not in "qe":
-        print(letter, end="")
+    if letter in 'qe':
+        continue
+    else:
+        print("{:c}".format(letter_code), end="")
