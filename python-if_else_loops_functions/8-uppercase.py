9ec7d2fb470e33320f76b1e3ab9c05b06feb6266
munhugues <h.munezero@alustudent.com>
2024-10-24 07:05:19 -0700

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(string):
+    for c in string:
+        o = ord(c)
+        if o in range(ord('a'),ord('z') + 1):
+            print("{}".format(chr(o - 32)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()
