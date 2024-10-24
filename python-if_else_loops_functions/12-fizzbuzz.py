5e38b0e9426eeed70587d26969b8c52c6bdefdf1
munhugues <h.munezero@alustudent.com>
2024-10-24 08:07:12 -0700

    12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    n = 1
+    while n<=100:
+        if n % 5 == 0 and n % 3 == 0:
+            print("FizzBuzz", end=" ")
+        elif n % 5 == 0:
+            print("Buzz", end=" ")
+        elif n % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print(n, end=" ")
+        n+=1
