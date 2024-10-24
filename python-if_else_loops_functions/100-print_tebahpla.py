e2b22b9a26c091df9c25d792c5c23b4473e0cc89
munhugues <h.munezero@alustudent.com>
2024-10-24 08:35:03 -0700

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):  # ASCII values of 'z' to 'a' are from 122 to 97
+for i in range(122, 96, -1): 
     if i % 2 == 0:
-        print("{}".format(chr(i)), end="")  # Even: lowercase
+        print("{}".format(chr(i)), end="")
     else:
-        print("{}".format(chr(i - 32)), end="")  # Odd: uppercase (subtract 32 from ASCII to get uppercase)
-
+        print("{}".format(chr(i - 32)), end="")
