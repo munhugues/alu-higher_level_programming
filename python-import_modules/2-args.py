835982615d8d46d0163dd8eb384b79333b39c30b
munhugues <h.munezero@alustudent.com>
2024-10-25 18:29:06 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-import sys
-args = sys.argv
-num = len(args)
-if num == 1:
-    print(f"{num - 1} arguments:")
+if __name__ == __main__:
+    import sys
+    args = sys.argv
+    num = len(args)
+    if num == 1:
+        print(f"{num - 1} arguments:")
 
-elif num == 2:
-    print(f"{num - 1} argument:")
-    for i, n in enumerate(sys.argv):
-        if i == 0:
-            continue
-        print(f"{i}: {n}")
+    elif num == 2:
+        print(f"{num - 1} argument:")
+        for i, n in enumerate(sys.argv):
+            if i == 0:
+                continue
+            print(f"{i}: {n}")
 
-else:
-    print(f"{num - 1} arguments:")
-    for i, n in enumerate(sys.argv):
-        if i == 0:
-            continue
-        print(f"{i}: {n}")
+    else:
+        print(f"{num - 1} arguments:")
+        for i, n in enumerate(sys.argv):
+            if i == 0:
+                continue
+            print(f"{i}: {n}")
