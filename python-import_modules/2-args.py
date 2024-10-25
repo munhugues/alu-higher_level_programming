6c214f4fa55f2198b7660d2cadab86ebbd6388f4
munhugues <h.munezero@alustudent.com>
2024-10-25 18:25:16 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 import sys
-for i, n in sys.argv():
-    print(f"{i} : {n}")
+args = sys.argv
+num = len(args)
+if num == 1:
+    print(f"{num - 1} arguments:")
+
+elif num == 2:
+    print(f"{num - 1} argument:")
+    for i, n in enumerate(sys.argv):
+        if i == 0:
+            continue
+        print(f"{i}: {n}")
+
+else:
+    print(f"{num - 1} arguments:")
+    for i, n in enumerate(sys.argv):
+        if i == 0:
+            continue
+        print(f"{i}: {n}")
