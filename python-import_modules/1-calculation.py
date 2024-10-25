906c5e80ca2231e412aacd9f81ae8b61d78c647e
munhugues <h.munezero@alustudent.com>
2024-10-25 08:21:37 -0700

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     import sys
     a = 10
     b = 5
-    sys.stderr.write("{} + {} = {}".format(a, b, add(a, b)))
+    sys.stdout.write("{} + {} = {}".format(a, b, add(a, b)))
     sys.stderr.write("{} - {} = {}".format(a, b, sub(a, b)))
     sys.stderr.write("{} * {} = {}".format(a, b, mul(a, b)))
     sys.stderr.write("{} / {} = {}".format(a, b, div(a, b)))
