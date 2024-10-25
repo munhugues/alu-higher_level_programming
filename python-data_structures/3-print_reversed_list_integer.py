fca71c8e12e3c56fb5b1a11b0d026b223019dd8c
munhugues <h.munezero@alustudent.com>
2024-10-25 11:38:20 -0700

    3-print_reversed_list_integer.py

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    tot = len(my_list) - 1
-    for i,n in enumerate(my_list):
-        print("{:d}".format(my_list[tot - i]))
+    if my_list:
+        for num in reversed(my_list):
+            print("{:d}".format(num))
