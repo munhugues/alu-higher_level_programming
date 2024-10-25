889690a6cae2d85c5235f2a4469fb141bbb3e436
munhugues <h.munezero@alustudent.com>
2024-10-25 11:17:10 -0700

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if (idx < 0) or (idx > len(my_list)):
-        print(my_list)
+        return my_list
     else:
         for i, n in enumerate(my_list):
             if i == idx :
