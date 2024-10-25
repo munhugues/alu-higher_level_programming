8f261d3ee0dd378dcca3d70d31bf08274bf86f54
munhugues <h.munezero@alustudent.com>
2024-10-25 08:05:36 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    val1 = 1
-    val2 = 2
-    print("{} + {} = {}".format(val1, val2, add(val1, val2)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
