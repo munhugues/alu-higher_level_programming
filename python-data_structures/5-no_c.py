e6e020ce317636b80d29d42a50bd964dbb6ddf46
munhugues <h.munezero@alustudent.com>
2024-10-25 12:10:41 -0700

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     result = ''
     for n in my_string:
-        if n == 'c' or n =='C':
+        if n == 'c' or n == 'C':
             continue
         result += n
     return result
