f1db21bc3e7d230c5fe29e58f21e995fda0b44ab
munhugues <h.munezero@alustudent.com>
2024-10-25 11:10:38 -0700

    2-replace_in_list.py

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if (idx < 0) or (idx > len(my_list)):
+        print(my_list)
+    else:
+        for i, n in my_list:
+            if i == idx:
+                my_list[idx] = element
+
+        return my_list
