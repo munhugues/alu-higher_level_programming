05efdcdbc5161ef56ad1400fe5770d8c9e01c255
munhugues <h.munezero@alustudent.com>
2024-11-05 08:36:49 -0800

    100-safe_print_integer_err.py

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        # Attempt to print the value as an integer
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        # Print the exception message to stderr
+        print("Exception:", e, file=sys.stderr)
+        return False
