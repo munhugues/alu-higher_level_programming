66c81bbc755789510fe9b961e8ab8a1defe751ef
munhugues <h.munezero@alustudent.com>
2024-11-05 00:17:31 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    t = 0
+    count = 0
     try:
-        for i, n in enumerate(my_list):
-            t += 1
-            print("{:d}".format(n), end="")
-            if i == x:
-                break
-        print()
-        return t
-
-    except ValueError:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+    except IndexError:
         pass
+    print()  # New line after the printed elements
+    return count
