a76945dbc4f7b4851d388bcf07b735eb90a83a26
munhugues <h.munezero@alustudent.com>
2024-11-07 05:35:16 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -23,6 +23,10 @@ class Square:
                 self.__size = size
 
     def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
         return self.__size * self.__size
 
     @sizesetter
