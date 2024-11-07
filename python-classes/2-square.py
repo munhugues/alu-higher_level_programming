6c81c264eab7cc3e4299115f2ea0dc883dd01d04
munhugues <h.munezero@alustudent.com>
2024-11-07 04:43:52 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,5 +5,11 @@
 class Square:
     """A class representing a square."""
     def __init__(self, size = 0):
-        self.__size = size
-        
+        if type(size) != int:
+            print("size must be an integer")
+
+        elif size < 0:
+            print("size must be >= 0")
+
+        else:
+            self.__size = size
